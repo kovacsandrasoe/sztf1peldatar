7102ceb740305a9c537dca1405f19f3b928dfbe4
CD1H49 <almasi.patrik@gmail.com>
2020-08-26 22:36:04 +0200

    Kieg 01 feladat - Negyzetszam

---
+++ modul01/patrik_K01-01_negyzetszam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem adjon meg egy egész számot: ");
+            int number = int.Parse(Console.ReadLine());
+
+            #region 1. Megoldás
+
+            int squareRoot = (int)Math.Sqrt(number);
+            Console.WriteLine("Négyzet szám? -> "+ (squareRoot*squareRoot==number));
+
+            #endregion
+            #region 2. Megoldás
+
+            Console.WriteLine("Négyzet szám? -> " + (Math.Sqrt(number) % 1 == 0));
+
+            #endregion
+            #region 3. Megoldás
+
+            Console.WriteLine("Négyzet szám? -> " + (Math.Sqrt(number) == (int)Math.Sqrt(number)));
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
