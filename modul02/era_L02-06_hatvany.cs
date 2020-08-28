11c8e5d4cabf1add46bc92510265aff094fd2eb3
Jakaberas <jakab.eras@gmail.com>
2020-08-28 12:50:22 +0200

    solution modified

--- modul02/era_L02-06_hatvany.cs
+++ modul02/era_L02-06_hatvany.cs
@@ -10,24 +10,23 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            double val;
-            double exponent;
-            double hatvanyeredmeny;
-            Console.Write("Add meg a számot, aminek a hatványát szeretnéd kiszámolni: ");
-            val = int.Parse(Console.ReadLine());
-            Console.Write("Add meg a kitevőt: ");
-            exponent = int.Parse(Console.ReadLine());
-            hatvanyeredmeny = Math.Pow(val, exponent);
-            Console.WriteLine("A " + val + " " + exponent + ". hatványa: " + hatvanyeredmeny);
-
             int szam;
-            int faktorialis = 1;
-            Console.Write("Melyik számnak szeretnéd megtudni a faktoriálisát? ");
+            int kitevo;
+            double hatvanyeredmeny;
+            Console.Write("Melyik számnak szeretnéd kiszámolni a hatványát és a faktoriálisát? ");
             szam = int.Parse(Console.ReadLine());
+            Console.Write("Add meg a hatványkitevőt: ");
+            kitevo = int.Parse(Console.ReadLine());
+            hatvanyeredmeny = Math.Pow(szam, kitevo);
+
+            Console.WriteLine("A(z) " + szam + " "+ kitevo + ". hatványa: " + hatvanyeredmeny);
+
+            double faktorialis = 1;
             for (int i = 1; i <= szam; i++)
             {
                 faktorialis = faktorialis * i;
             }
+
             Console.WriteLine("A(z) " + szam + " faktoriálisa: " + faktorialis);
             Console.ReadLine();
         }
