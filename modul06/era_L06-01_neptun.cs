bd68e8049189bb68fa87e6895d50b76c5f740aea
Jakaberas <jakab.eras@gmail.com>
2020-09-04 10:44:14 +0200

    solution added

---
+++ modul06/era_L06-01_neptun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JatekAStringekkel_L3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // neptun kód generálás
+            Random r = new Random();
+
+            string neptunKod = "";
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (r.Next(2) == 0) // 0 vagy 1
+                {
+                    neptunKod += r.Next(0, 10);
+                }
+                else
+                {
+                    neptunKod += (char)r.Next('A', ((int)'Z') + 1); // a +1 amiatt kell, hogy a Z karaktere is benne legyen a kimenetben
+                }
+            }
+
+            Console.WriteLine("Az előállított neptunkód: {0}", neptunKod);
+            Console.ReadLine();
+        }
+    }
+}
